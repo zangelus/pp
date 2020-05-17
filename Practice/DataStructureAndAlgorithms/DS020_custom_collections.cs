b27e9163ba4634f2e57c5bdbe02f73f1c9de1620
Zbigniew Angelus <zangelus@gmail.com>
2020-05-17 08:56:26 -0400

    test implementention of IEnumerable<T>

--- Practice/DataStructureAndAlgorithms/DS020_custom_collections.cs
+++ Practice/DataStructureAndAlgorithms/DS020_custom_collections.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Mail;
 using System.Text;
 
@@ -28,6 +29,13 @@ namespace DataStructureAndAlgorithms
         public string Name { get; set; }
     }
 
+    /* IEnumerable example
+    **
+    **
+    **
+    **
+    **
+    */
 
     public class People : IEnumerable
     {
@@ -45,10 +53,10 @@ namespace DataStructureAndAlgorithms
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return GetEnumerator();
+            return GetPeopleEnumerator();
         }
 
-        public IEnumerator GetEnumerator()
+        public IEnumerator GetPeopleEnumerator()
         {
             return new PeopleEnumerator(_list);
         }
@@ -68,7 +76,14 @@ namespace DataStructureAndAlgorithms
         {
             get
             {
-                return _list[currentPosition];
+                try
+                {
+                    return _list[currentPosition];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
             }
         }
 
@@ -91,7 +106,117 @@ namespace DataStructureAndAlgorithms
         }
     }
 
+    /* IEnumerable<T> example
+    **
+    **
+    **
+    **
+    **
+    */
 
+    public class StreamReaderEnumerable : IEnumerable<string>
+    {
+
+        string _path;
+
+        public StreamReaderEnumerable(string path)
+        {
+            _path = path;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+             return new StreamReaderEnumerator(_path);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator1();
+        }
+
+        private IEnumerator GetEnumerator1()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class StreamReaderEnumerator : IEnumerator<string>
+    {
+        private StreamReader _sr;
+        public StreamReaderEnumerator(string path)
+        {
+            _sr = new StreamReader(path);
+        }
+
+        private string _current;
+
+        public string Current
+        {
+            get
+            {
+                if(_sr == null || _current == null)
+                {
+                    throw new InvalidOperationException();
+                    
+                }
+
+                return _current;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            _current = _sr.ReadLine();
+            return _current != null;
+        }
+
+        public void Reset()
+        {
+            _sr.DiscardBufferedData();
+            _sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            _current = null;
+        }
+
+        // Implement IDisposable, which is also implemented by IEnumerator(T).
+        private bool disposedValue = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    // Dispose of managed resources.
+                }
+                _current = null;
+                if (_sr != null)
+                {
+                    _sr.Close();
+                    _sr.Dispose();
+                }
+            }
+
+            this.disposedValue = true;
+        }
+
+        ~StreamReaderEnumerator()
+        {
+            Dispose(false);
+        }
+    }
 
 
     //references to add to readme
--- Practice/Problems.UnitTest/DS020_UT.cs
+++ Practice/Problems.UnitTest/DS020_UT.cs
@@ -4,6 +4,14 @@ using System.Text;
 using NUnit.Framework;
 using DataStructureAndAlgorithms;
 using System.Diagnostics;
+using System.Resources;
+using System.ComponentModel;
+using System.Reflection;
+using System.IO;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
+using System.Security.Cryptography;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace Problems.UnitTest
 {
@@ -11,23 +19,23 @@ namespace Problems.UnitTest
     class DS020_UT
     {
         public DS020_custom_collections problem;
-        
+
         [SetUp]
         public void Setup()
         {
             problem = new DS020_custom_collections();
         }
 
-        [Test] 
+        [Test]
         public void TestIterationPattern()
         {
             People personList = new People(
-                new[] 
+                new[]
                 {
                     new Person() { Name = "Name1", ID = 1 },
                     new Person() { Name = "Name2", ID = 2 },
                     new Person() { Name = "Name3", ID = 3 },
-                    new Person() { Name = "Name4", ID = 4 } 
+                    new Person() { Name = "Name4", ID = 4 }
                 });
 
             Console.WriteLine("gi there");
@@ -36,4 +44,40 @@ namespace Problems.UnitTest
                 Console.WriteLine("Name: " + person.Name + " ID: " + person.ID);
             }
         }
+
+        [Test]
+        public void TestIterationPattern2()
+        {
+            ResourceManager rm = new ResourceManager("Problems.UnitTest.Resource1", Assembly.GetExecutingAssembly());
+
+            string fileName = "Test1.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Resource\", fileName);
+
+            StreamReaderEnumerable sre = new StreamReaderEnumerable(path);
+
+            IEnumerable<string> results;
+
+            try
+            {
+                results = from line in new StreamReaderEnumerable(path)
+                          where line.Contains("1500")
+                          select line;
+            }
+            catch
+            {
+                throw new FileNotFoundException();
+            }
+
+            int count = 0;
+            foreach(string line in results)
+            {
+                Console.WriteLine("Line"+ ++count + ": " + line);
+            }
+            
+
+            //string text = File.ReadAllText(path);
+            //Console.WriteLine(text);
+        }
+
+    }
 }
